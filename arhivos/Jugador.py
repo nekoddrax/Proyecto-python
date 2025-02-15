644fc16cba99938fcf020d6294665c53a5a0df7a
nekoddrax <chispita24035@gmail.com>
2025-02-14 22:58:35 -0300

    si

---
+++ arhivos/Jugador.py
@@ -0,0 +1,8 @@
+# jugador
+vida_jugador = 100
+daño_jugador = 10
+velocidad_jugador = 10
+
+
+
+inventario = {"pocion": 1}
--- arhivos/juego.py
+++
@@ -1,7 +0,0 @@
-# Menu de juego iniciado
-#teste de pelea de monstruos
-form Adventure_text import mostrar_menu
-#Stats del jugador
-vida_jugador = 100
-daño_jugador = 10
-inventario = {"pocion": 1}
