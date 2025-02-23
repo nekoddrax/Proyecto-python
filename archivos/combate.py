e2e6171c753174c5ff8c4a5ac22774e9b1b1c94b
nekoddrax <chispita24035@gmail.com>
2025-02-23 20:07:51 -0300

    si

--- archivos/combate.py
+++ archivos/combate.py
@@ -6,38 +6,45 @@ import time
 def limpar_pantalla():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+turno = 1
 # Crear instancias de Jugador y Enemigo
 jugador = Player.Jugador()
 enemigo = Enemy.Enemigo()
-while True:
+while True: 
     print("Vida del jugador: ", jugador.vida_jugador, "\n")
-   
-    accion = input("Que quieres hacer? \n \n 1. Atacar \n 2. Defender \n 3. Usar poción \n")
-    if accion == "1":
-        print("\n")  
-        jugador.atacar()
+    def turno_jugador(): 
+        accion = input("Que quieres hacer? \n \n 1. Atacar \n 2. Defender \n 3. Usar poción \n")
+        if accion == "1":
+            print("\n")  
+            time.sleep(2)
+        elif accion == "2":
+            print("\n")
+            jugador.defender()
+            time.sleep(2)
+        elif accion == "3":
+            print("\n")
+            jugador.usar_pocion()
+            time.sleep(2)
+        else:
+            print("Comando no válido")
+            time.sleep(1)
+
+    def turno_enemigo():
+        jugador.vida_jugador -= enemigo.daño_enemigo
         time.sleep(2)
         limpar_pantalla()
-    elif accion == "2":
-        print("\n")
-        jugador.defender()
-        time.sleep(2)
+
+
+    if turno == 1:
+        turno_jugador()
         limpar_pantalla()
-    elif accion == "3":
-        print("\n")
-        jugador.usar_pocion()
-        time.sleep(2)
-        limpar_pantalla()
-    elif accion == "4":
-        jugador.vida_jugador = 0
-    
-    
-    else:
-        print("Comando no válido")
-        time.sleep(2)
-        limpar_pantalla()
-   
+        turno = 2
+    elif turno == 2:
+        turno_enemigo()
+        turno = 1
+
     if jugador.vida_jugador <= 0:
         limpar_pantalla()
         print("Has perdido")
+        time.sleep(2)
         break
